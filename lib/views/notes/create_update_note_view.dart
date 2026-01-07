a71cff29a5edec969d743364b198ebeba7cfeda8
Koushik Saha <csekoushik2001@gmail.com>
2026-01-08 00:43:22 +0600

    thirteen  commit

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -5,6 +5,9 @@ import 'package:mynotesh/services/cloud/cloud_note.dart';
 import 'package:mynotesh/services/cloud/cloud_storage_exceptions.dart';
 import 'package:mynotesh/services/cloud/cloud_storage_constants.dart';
 import 'package:mynotesh/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
+import '../../utilities/dialogs/cannot_share_empty_note_dialog.dart';
+
 
 
 class CreateUpdateNoteView extends StatefulWidget {
@@ -93,6 +96,20 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: Text('New Note'),
+        actions: [
+          IconButton(
+              onPressed: () async{
+               final text = _textController.text;
+               if (_note == null || text.isEmpty){
+                 await showCannotShareEmptyNoteDialog(context);
+               }
+               else{
+                 Share.share(text);
+               }
+              },
+              icon: const Icon(Icons.share),
+          )
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
